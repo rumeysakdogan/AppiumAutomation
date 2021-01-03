f8aea137bec0945396dafa96033c7a7f8173221c
rumeysakdogan <dgn.rumeysaa@gmail.com>
2021-01-03 13:15:31 -0700

    added ChromeBrowser test

--- src/test/java/com/cybertek/tests/ChromeBrowserTest.java
+++ src/test/java/com/cybertek/tests/ChromeBrowserTest.java
@@ -1,4 +1,50 @@
 package com.cybertek.tests;
 
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class ChromeBrowserTest {
+
+    AppiumDriver<AndroidElement> driver;
+
+    @Before
+    public void setUp() throws MalformedURLException {
+
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, Platform.ANDROID);
+        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "7.0");
+        desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel_2");
+        desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+        desiredCapabilities.setCapability(AndroidMobileCapabilityType.BROWSER_NAME, "Chrome");
+
+        URL url = new URL("http://localhost:4723/wd/hub");
+        driver = new AndroidDriver<>(url, desiredCapabilities);
+    }
+
+    @After
+    public void tearDown() {
+        driver.closeApp();
+    }
+
+    @Test
+    public void chromeBrowserTest() throws InterruptedException {
+
+        driver.get("https://qa1.vytrack.com");
+        driver.findElementById("prependedInput").sendKeys("storemanager85");
+        driver.findElementById("prependedInput2").sendKeys("UserUser123" + Keys.ENTER);
+        Thread.sleep(3000);
+    }
 }
